ff27c2fcbf04d31b21793e6a2f8e3be80b710803
Ikuo Hirakawa <ikuo.hirakawa@i-studio.co.jp>
2021-06-14 17:53:41 +0900

    :construction:

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const ngrok = require('ngrok')
 const axios = require('axios')
 const crypto = require('crypto')
+const { IncomingWebhook } = require('@slack/webhook');
+
 const bodyParser = require("body-parser")
 const endpoint = "https://heroku-test-figma.herokuapp.com"
+
+const slackURL = process.env.SLACK_WEBHOOK_URL;
+const webhook = new IncomingWebhook(url);
+
 
 // Initialize express and define a port
 const app = express()
@@ -13,12 +19,13 @@ app.use(bodyParser.json())
 
 app.post('/', (request, response) => {
   const { body } = request
-  console.log(body)
   if (body.passcode === passcode) {
-    const { timestamp } = body
+    const { comment, file_key, file_name, mentions, triggerd_by, timestamp } = body
+    notifySlack({comment, file_key, file_name, mentions, triggerd_by})
     console.log(`comment was updated at ${timestamp}`)
     response.sendStatus(200)
   } else {
+    console.log(`inValid`)
     response.sendStatus(403)
   }
 })
@@ -26,6 +33,7 @@ app.post('/', (request, response) => {
 // Start express on the defined port
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 
+// toDo DBにIDを保存
 ngrok.connect(PORT).then(async () => {
   const response = await axios({
     url: 'https://api.figma.com/v2/webhooks',
@@ -42,3 +50,41 @@ ngrok.connect(PORT).then(async () => {
   })
   console.log(`🎣 Webhook ${response.data.id} successfully created`)
 })
+
+const notifySlack = ({
+  comment,
+  file_key,
+  file_name,
+  mentions,
+  triggerd_by
+}) => {
+  const triggerdName = triggerd_by.handle
+  const url = parseFigmaUrl(file_key)
+  const commentText = comment.map((n) => {
+    return n.text
+  })
+  (async () => {
+    await webhook.send(
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `
+            ${triggerdName} がコメントをしました【<${}|${file_name}】
+          `
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `
+            ${commentText}
+          `
+      }
+    );
+  })();  
+}
+
+const parseFigmaUrl = (file_key) => {
+  return `figma.com/file/${file_key}/`
+}
