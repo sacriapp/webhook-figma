66cd852dcd20a873bd942cb367ea0c049cbf8ce8
Ikuo Hirakawa <ikuo.hirakawa@i-studio.co.jp>
2021-06-14 16:23:08 +0900

    :construction: figma api call

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 const express = require('express')
-const path = require('path')
+const ngrok = require('ngrok')
+const axios = require('axios')
+const crypto = require('crypto')
 const bodyParser = require("body-parser")
+const endpoint = "https://heroku-test-figma.herokuapp.com/figma"
 
 // Initialize express and define a port
 const app = express()
 const PORT = process.env.PORT || 5000
-// Tell express to use body-parser's JSON parsing
+const passcode = crypto.randomBytes(48).toString('hex')
 app.use(bodyParser.json())
+
+app.post('/', (request, response) => {
+  if (request.body.passcode === passcode) {
+    const { file_name, timestamp } = request.body
+    console.log(`${file_name} was updated at ${timestamp}`)
+    response.sendStatus(200)
+  } else {
+    response.sendStatus(403)
+  }
+})
 
 app.post('/figma', (req, res) => {
   const body = req.body;
-  console.log(req, body) // Call your action on the request here
+  console.log(body) // Call your action on the request here
   res.status(200).end()
 })
-
-// Server index page
-app.get("/", function (req, res) {
-  res.send("Deployed!");
-});
 
 // Start express on the defined port
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+ngrok.connect(PORT).then(async () => {
+  const response = await axios({
+    url: 'https://api.figma.com/v2/webhooks',
+    method: 'post',
+    headers: {
+      'X-Figma-Token': process.env.FIGMA_TOKEN,
+    },
+    data: {
+      event_type: 'FILE_COMMENT',
+      team_id: '829209809582428090',
+      passcode,
+      endpoint,
+    },
+  })
+  console.log(`🎣 Webhook ${response.data.id} successfully created`)
+})
