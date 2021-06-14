5a335e7c36f7234d0ca9cc2e29904110baa3081b
Ikuo Hirakawa <ikuo.hirakawa@i-studio.co.jp>
2021-06-14 15:57:58 +0900

    :construction:

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const bodyParser = require("body-parser")
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Initialize express and define a port
+const app = express()
+const PORT = process.env.PORT || 5000
+// Tell express to use body-parser's JSON parsing
+app.use(bodyParser.json())
+
+app.get('/figma', (req, res) => {
+  console.log(req.body) // Call your action on the request here
+  res.status(200).end() // Responding is important
+})
+
+// Server index page
+app.get("/", function (req, res) {
+  res.send("Deployed!");
+});
+
+// Start express on the defined port
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
